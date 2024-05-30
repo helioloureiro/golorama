00c9cbec5802266aae55615b3139938d7624bc10
Helio Loureiro <helio.loureiro@ericsson.com>
2024-05-30 15:41:02 +0200

    Improved handling
     - remove the _EX from ligth colors
     - made bright the same as bold

--- golorama.go
+++ golorama.go
@@ -10,6 +10,7 @@ const (
 
 const (
 	BRIGHT    = 1
+	BOLD      = 1
 	DIM       = 2
 	NORMAL    = 22
 	RESET_ALL = 0
@@ -38,15 +39,25 @@ const (
 	LIGHTWHITE_EX
 )
 
+const (
+	LIGHTBLACK = iota + 90
+	LIGHTRED
+	LIGHTGREEN
+	LIGHTYELLOW
+	LIGHTBLUE
+	LIGHTMAGENTA
+	LIGHTCYAN
+	LIGHTWHITE
+)
+
 // GetCSI: to get the color as termio format
 func GetCSI(color int) string {
-	colorTAG := fmt.Sprintf("%s%dm", CSI, color)
-	return colorTAG
+	return fmt.Sprintf("%s%dm", CSI, color)
 }
 
 // Reset: set color off to the terminal
 func Reset() string {
-	return "\033[0m"
+	return GetCSI(RESET_ALL)
 }
 
 // PrintColorln: a direct call to print lines in colorful way
